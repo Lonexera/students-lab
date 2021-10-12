873de6d09af0d892423560d6f45afee37a03024f
Tatsiana <tatianatolkachyova@gmail.com>
2021-10-12 16:21:38 +0300

    fix: fix commented issues

--- app/src/main/java/com/example/plantsapp/MainActivity.kt
+++ app/src/main/java/com/example/plantsapp/MainActivity.kt
@@ -7,29 +7,18 @@ import com.example.plantsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-
-                R.id.bottom_nav_tasks -> {
-                    openFragment(TasksFragment())
-                    true
-                }
-
-                R.id.bottom_nav_plants -> {
-                    openFragment(PlantsFragment())
-                    true
-                }
-
-                else -> true
+                R.id.bottom_nav_tasks -> openFragment(TasksFragment())
+                R.id.bottom_nav_plants -> openFragment(PlantsFragment())
             }
+            true
         }
     }
 
