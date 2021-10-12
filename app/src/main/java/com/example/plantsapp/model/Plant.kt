2b10d0e118eb40309010baedd67efb662305a47f
Tatsiana <tatianatolkachyova@gmail.com>
2021-10-12 16:51:55 +0300

    feat: create layout for plants screen

--- app/src/main/java/com/example/plantsapp/model/Plant.kt
+++ app/src/main/java/com/example/plantsapp/model/Plant.kt
@@ -3,6 +3,6 @@ package com.example.plantsapp.model
 data class Plant(
     val name: String,
     val speciesName: String,
-    val plantPicture: String,
+    // val plantPicture: String,
     val wateringFrequency: Int
 )
