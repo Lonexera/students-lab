cda2f44529b9d6d4b2d5e60d6c2563aef3aa3104
Tatsiana <tatianatolkachyova@gmail.com>
2021-10-12 15:05:24 +0300

    feat: implement switching between fragments

--- app/src/main/java/com/example/plantsapp/MainActivity.kt
+++ app/src/main/java/com/example/plantsapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.plantsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +11,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigation.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+
+                R.id.bottom_nav_tasks -> {
+                    openFragment(TasksFragment())
+                    true
+                }
+
+                R.id.bottom_nav_plants -> {
+                    openFragment(PlantsFragment())
+                    true
+                }
+
+                else -> true
+            }
+        }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
