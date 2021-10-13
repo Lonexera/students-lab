df092b0211883a037f8847112d9b12eb259dd233
Tatsiana <tatianatolkachyova@gmail.com>
2021-10-13 17:04:17 +0300

    feat: add PlantDetailViewModel + implement FragmentPlantDetail

--- app/src/main/java/com/example/plantsapp/view/PlantDetailFragment.kt
+++ app/src/main/java/com/example/plantsapp/view/PlantDetailFragment.kt
@@ -5,12 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.example.plantsapp.R
 import com.example.plantsapp.databinding.FragmentPlantDetailBinding
+import com.example.plantsapp.model.Plant
+import com.example.plantsapp.viewModels.PlantDetailViewModel
 
 class PlantDetailFragment : Fragment() {
 
     private var _binding: FragmentPlantDetailBinding? = null
     private val binding get() = _binding!!
+    private val detailViewModel: PlantDetailViewModel by viewModels()
+    private lateinit var detailPlant: Plant
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +30,25 @@ class PlantDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        with(detailViewModel) {
+            plant.observe(viewLifecycleOwner) {
+                detailPlant = it
+            }
+        }
+
+        with(binding) {
+            Glide.with(requireContext())
+                .load(detailPlant.plantPicture)
+                .into(ivPlant)
+
+            tvPlantName.text = detailPlant.name
+            tvSpeciesName.text = detailPlant.speciesName
+            tvWateringValue.text = getString(
+                R.string.msg_watering_frequency_value,
+                detailPlant.wateringFrequencyDays
+            )
+        }
     }
 
     override fun onDestroyView() {
