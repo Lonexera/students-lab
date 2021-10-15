c29082894f91e864d2e81a2a966aed89f71fa712
Tatsiana <tatianatolkachyova@gmail.com>
2021-10-15 12:53:15 +0300

    refactor: fix detekt issues

--- app/src/main/java/com/example/plantsapp/view/PlantsFragment.kt
+++ app/src/main/java/com/example/plantsapp/view/PlantsFragment.kt
@@ -1,9 +1,7 @@
 package com.example.plantsapp.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
--- app/src/main/java/com/example/plantsapp/view/TasksFragment.kt
+++ app/src/main/java/com/example/plantsapp/view/TasksFragment.kt
@@ -1,16 +1,9 @@
 package com.example.plantsapp.view
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.plantsapp.R
-import com.example.plantsapp.databinding.FragmentTasksBinding
 
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
-    private val binding: FragmentTasksBinding by viewBinding(FragmentTasksBinding::bind)
-
+    // private val binding: FragmentTasksBinding by viewBinding(FragmentTasksBinding::bind)
 }
