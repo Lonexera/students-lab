a53b072d73329aabb259144385227f04e41d020a
Tatsiana <tatianatolkachyova@gmail.com>
2021-10-15 13:46:52 +0300

    refactor: delete commented line + change view binding delegate library version

--- app/src/main/java/com/example/plantsapp/view/TasksFragment.kt
+++ app/src/main/java/com/example/plantsapp/view/TasksFragment.kt
@@ -3,7 +3,4 @@ package com.example.plantsapp.view
 import androidx.fragment.app.Fragment
 import com.example.plantsapp.R
 
-class TasksFragment : Fragment(R.layout.fragment_tasks) {
-
-    // private val binding: FragmentTasksBinding by viewBinding(FragmentTasksBinding::bind)
-}
+class TasksFragment : Fragment(R.layout.fragment_tasks)
