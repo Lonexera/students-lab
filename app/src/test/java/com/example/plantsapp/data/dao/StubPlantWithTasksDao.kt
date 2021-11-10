0f20a59899cc992dd1a41c939bd7b6bad85efc25
Tatsiana <tatianatolkachyova@gmail.com>
2021-11-10 17:33:10 +0300

    fix tests

--- app/src/test/java/com/example/plantsapp/data/dao/StubPlantWithTasksDao.kt
+++ app/src/test/java/com/example/plantsapp/data/dao/StubPlantWithTasksDao.kt
@@ -1,24 +1,17 @@
 package com.example.plantsapp.data.dao
 
-import com.example.plantsapp.data.entity.RoomPlant
 import com.example.plantsapp.data.entity.RoomPlantWithTasks
 import com.example.plantsapp.data.entity.RoomTask
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 internal class StubPlantWithTasksDao(
-    private val plants: List<RoomPlant>,
-    private val tasks: List<RoomTask>
+    private val plantWithTasks: List<RoomPlantWithTasks>
 ) : RoomPlantWithTasksDao {
 
     override fun getPlantsWithTasks(): Flow<List<RoomPlantWithTasks>> =
         flow {
-            plants.map { plant ->
-                RoomPlantWithTasks(
-                    plant,
-                    tasks.filter { it.plantName == plant.name }
-                )
-            }
+            emit(plantWithTasks)
         }
 
     override suspend fun insert(tasks: List<RoomTask>) {
--- app/src/test/java/com/example/plantsapp/data/repository/RoomTasksRepositoryTest.kt
+++ app/src/test/java/com/example/plantsapp/data/repository/RoomTasksRepositoryTest.kt
@@ -1,15 +1,15 @@
 package com.example.plantsapp.data.repository
 
 import com.example.plantsapp.data.dao.StubPlantWithTasksDao
-import com.example.plantsapp.data.entity.TaskKeys
+import com.example.plantsapp.data.entity.RoomPlantWithTasks
 import com.example.plantsapp.data.utils.createPlant
 import com.example.plantsapp.data.utils.createRoomPlant
 import com.example.plantsapp.data.utils.createRoomTask
 import com.example.plantsapp.domain.model.Plant
 import com.example.plantsapp.domain.model.Task
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.util.Calendar
@@ -20,154 +20,148 @@ class RoomTasksRepositoryTest {
     private val creationDate = Calendar.getInstance().apply {
         set(2021, 11, 10)
     }.time
-    private val defaultPlants = listOf(
-        createRoomPlant(
-            name = "Greg",
-            creationDateMillis = creationDate.time
+    private val plantsWithTasks = listOf(
+        RoomPlantWithTasks(
+            createRoomPlant(name = "Greg", creationDateMillis = creationDate.time),
+            listOf(
+                createRoomTask(
+                    plantName = "Greg",
+                    frequency = 11
+                ),
+                createRoomTask(
+                    plantName = "Greg",
+                    frequency = 2
+                )
+            )
         ),
-        createRoomPlant(
-            name = "Roger",
-            creationDateMillis = creationDate.time
-        )
-    )
-    private val defaultTasks = listOf(
-        createRoomTask(
-            taskKey = TaskKeys.WATERING_TASK,
-            plantName = "Greg",
-            frequency = 11
-        ),
-        createRoomTask(
-            taskKey = TaskKeys.SPRAYING_TASK,
-            plantName = "Greg",
-            frequency = 2
-        ),
-        createRoomTask(
-            taskKey = TaskKeys.SPRAYING_TASK,
-            plantName = "Roger",
-            frequency = 6
-        ),
-        createRoomTask(
-            taskKey = TaskKeys.LOOSENING_TASK,
-            plantName = "Roger",
-            frequency = 12
+        RoomPlantWithTasks(
+            createRoomPlant(name = "Roger", creationDateMillis = creationDate.time),
+            listOf(
+                createRoomTask(
+                    plantName = "Roger",
+                    frequency = 6
+                ),
+                createRoomTask(
+                    plantName = "Roger",
+                    frequency = 12
+                )
+            )
         )
     )
 
     @Test
-    fun `tasks for creation date - returns all tasks`() {
-        TestCoroutineScope().launch {
-            val dao = StubPlantWithTasksDao(defaultPlants, defaultTasks)
+    fun `tasks for creation date - returns all tasks`() = runBlockingTest {
+        val dao = StubPlantWithTasksDao(plantsWithTasks)
 
-            val result = RoomTasksRepository(dao).getPlantsWithTasksForDate(creationDate)
+        val result = RoomTasksRepository(dao).getPlantsWithTasksForDate(creationDate)
 
-            val expectedResult = listOf(
-                createPlant(
-                    name = "Greg"
-                ) to listOf(
-                    Task.WateringTask(11),
-                    Task.SprayingTask(2)
-                ),
-                createPlant(
-                    name = "Roger"
-                ) to listOf(
-                    Task.SprayingTask(5),
-                    Task.LooseningTask(12)
-                )
+        val expectedResult = listOf(
+            createPlant(
+                name = "Greg"
+            ) to listOf(
+                Task.WateringTask(11),
+                Task.WateringTask(2)
+            ),
+            createPlant(
+                name = "Roger"
+            ) to listOf(
+                Task.WateringTask(6),
+                Task.WateringTask(12)
             )
-            assertEquals(expectedResult, result)
+        )
+        result.collect {
+            assertEquals(expectedResult, it)
         }
     }
 
     @Test
-    fun `empty plants and tasks lists - returns empty list`() {
-        TestCoroutineScope().launch {
-            val dao = StubPlantWithTasksDao(emptyList(), emptyList())
+    fun `no fitting tasks for date - returns empty list`() = runBlockingTest {
+        val date = Calendar.getInstance().apply {
+            set(2021, 11, 11)
+        }.time
+        val dao = StubPlantWithTasksDao(plantsWithTasks)
 
-            val result = RoomTasksRepository(dao).getPlantsWithTasksForDate(creationDate)
+        val result = RoomTasksRepository(dao).getPlantsWithTasksForDate(date)
 
-            val expectedResult = emptyList<Pair<Plant, List<Task>>>()
-            assertEquals(expectedResult, result)
+        val expectedResult = emptyList<Pair<Plant, List<Task>>>()
+        result.collect {
+            assertEquals(expectedResult, it)
         }
     }
 
     @Test
-    fun `empty tasks lists - returns empty list`() {
-        TestCoroutineScope().launch {
-            val dao = StubPlantWithTasksDao(defaultPlants, emptyList())
+    fun `tasks for date after creation date - returns list of fitting tasks`() = runBlockingTest {
+        val date = Calendar.getInstance().apply {
+            set(2021, 11, 22)
+        }.time
+        val dao = StubPlantWithTasksDao(plantsWithTasks)
 
-            val result = RoomTasksRepository(dao).getPlantsWithTasksForDate(creationDate)
+        val result = RoomTasksRepository(dao).getPlantsWithTasksForDate(date)
 
-            val expectedResult = emptyList<Pair<Plant, List<Task>>>()
-            assertEquals(expectedResult, result)
-        }
-    }
-
-    @Test
-    fun `no fitting tasks for date - returns empty list`() {
-        TestCoroutineScope().launch {
-            val date = Calendar.getInstance().apply {
-                set(2021, 11, 11)
-            }.time
-
-            val dao = StubPlantWithTasksDao(defaultPlants, defaultTasks)
-
-            val result = RoomTasksRepository(dao).getPlantsWithTasksForDate(date)
-
-            val expectedResult = emptyList<Pair<Plant, List<Task>>>()
-            assertEquals(expectedResult, result)
-        }
-    }
-
-    @Test
-    fun `tasks for date after creation date - returns list of fitting tasks`() {
-        TestCoroutineScope().launch {
-            val date = Calendar.getInstance().apply {
-                set(2021, 11, 22)
-            }.time
-            val dao = StubPlantWithTasksDao(defaultPlants, defaultTasks)
-
-            val result = RoomTasksRepository(dao).getPlantsWithTasksForDate(date)
-
-            val expectedResult = listOf(
-                createPlant(
-                    name = "Greg"
-                ) to listOf(
-                    Task.SprayingTask(2)
-                ),
-                createPlant(
-                    name = "Roger"
-                ) to listOf(
-                    Task.LooseningTask(12)
-                )
+        val expectedResult = listOf(
+            createPlant(
+                name = "Greg"
+            ) to listOf(
+                Task.WateringTask(2)
+            ),
+            createPlant(
+                name = "Roger"
+            ) to listOf(
+                Task.WateringTask(6),
+                Task.WateringTask(12)
             )
-            assertEquals(expectedResult, result)
+        )
+        result.collect {
+            assertEquals(expectedResult, it)
         }
     }
 
     @Test
-    fun `tasks for date before creation date - returns list of fitting tasks`() {
-        TestCoroutineScope().launch {
-            val date = Calendar.getInstance().apply {
-                set(2021, 11, 4)
-            }.time
+    fun `tasks for date before creation date - returns empty list`() = runBlockingTest {
+        val date = Calendar.getInstance().apply {
+            set(2021, 11, 8)
+        }.time
+        val dao = StubPlantWithTasksDao(plantsWithTasks)
 
-            val dao = StubPlantWithTasksDao(defaultPlants, defaultTasks)
+        val result = RoomTasksRepository(dao).getPlantsWithTasksForDate(date)
 
-            val result = RoomTasksRepository(dao).getPlantsWithTasksForDate(date)
+        val expectedResult = emptyList<Pair<Plant, List<Task>>>()
+        result.collect {
+            assertEquals(expectedResult, it)
+        }
+    }
 
-            val expectedResult = listOf(
-                createPlant(
-                    name = "Greg"
-                ) to listOf(
-                    Task.SprayingTask(2)
-                ),
-                createPlant(
-                    name = "Roger"
-                ) to listOf(
-                    Task.SprayingTask(6)
-                )
+    @Test
+    fun `empty plants and tasks lists - returns empty list`() = runBlockingTest {
+        val dao = StubPlantWithTasksDao(emptyList())
+
+        val result = RoomTasksRepository(dao).getPlantsWithTasksForDate(creationDate)
+
+        val expectedResult = emptyList<Pair<Plant, List<Task>>>()
+        result.collect {
+            assertEquals(expectedResult, it)
+        }
+    }
+
+    @Test
+    fun `empty tasks lists - returns empty list`() = runBlockingTest {
+        val plantsWithTasksList = listOf(
+            RoomPlantWithTasks(
+                createRoomPlant(name = "Greg", creationDateMillis = creationDate.time),
+                emptyList()
+            ),
+            RoomPlantWithTasks(
+                createRoomPlant(name = "Roger", creationDateMillis = creationDate.time),
+                emptyList()
             )
-            assertEquals(expectedResult, result)
+        )
+        val dao = StubPlantWithTasksDao(plantsWithTasksList)
+
+        val result = RoomTasksRepository(dao).getPlantsWithTasksForDate(creationDate)
+
+        val expectedResult = emptyList<Pair<Plant, List<Task>>>()
+        result.collect {
+            assertEquals(expectedResult, it)
         }
     }
 }
