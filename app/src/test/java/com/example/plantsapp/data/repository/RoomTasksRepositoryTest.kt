666a61c8c4c4d3ab064bf13556079137549ab439
Tatsiana <tatianatolkachyova@gmail.com>
2021-11-15 12:40:02 +0300

    fix: refactor tests

--- app/src/test/java/com/example/plantsapp/data/repository/RoomTasksRepositoryTest.kt
+++ app/src/test/java/com/example/plantsapp/data/repository/RoomTasksRepositoryTest.kt
@@ -47,8 +47,8 @@ class RoomTasksRepositoryTest {
         val actual = RoomTasksRepository(dao)
             .getPlantsWithTasksForDate(creationDate)
             .first()
-            .map { it.second }
-            .flatten()
+            .map { (_, tasks) -> tasks }
+            .first()
 
         val expectedResult = listOf(
             Task.WateringTask(11),
@@ -81,10 +81,8 @@ class RoomTasksRepositoryTest {
         val actual = RoomTasksRepository(dao)
             .getPlantsWithTasksForDate(todayDate)
             .first()
-            .map { it.second }
-            .flatten()
 
-        val expectedResult = emptyList<Task>()
+        val expectedResult = emptyList<Pair<Plant, List<Task>>>()
         assertEquals(expectedResult, actual)
     }
 
@@ -105,19 +103,6 @@ class RoomTasksRepositoryTest {
                         frequency = 2
                     )
                 )
-            ),
-            RoomPlantWithTasks(
-                createRoomPlant(creationDateMillis = creationDate.time),
-                listOf(
-                    createRoomTask(
-                        taskKey = TaskKeys.WATERING_TASK,
-                        frequency = 4
-                    ),
-                    createRoomTask(
-                        taskKey = TaskKeys.LOOSENING_TASK,
-                        frequency = 3
-                    )
-                )
             )
         )
         val dao = StubPlantWithTasksDao(initialList)
@@ -125,74 +110,32 @@ class RoomTasksRepositoryTest {
         val actual = RoomTasksRepository(dao)
             .getPlantsWithTasksForDate(date)
             .first()
-            .map { it.second }
-            .flatten()
+            .map { (_, tasks) -> tasks }
+            .first()
 
         val expectedResult = listOf(
             Task.SprayingTask(2),
-            Task.WateringTask(4),
-            Task.LooseningTask(3)
         )
         assertEquals(expectedResult, actual)
     }
 
     @Test
-    fun `tasks for date before creation date - returns list of fitting tasks`() =
-        runBlockingTest {
-            val date = createDate(2021, 11, 8)
-            val creationDate = createDate(2021, 11, 10)
-            val initialList = listOf(
-                RoomPlantWithTasks(
-                    createRoomPlant(creationDateMillis = creationDate.time),
-                    listOf(
-                        createRoomTask(
-                            taskKey = TaskKeys.WATERING_TASK,
-                            frequency = 11
-                        ),
-                        createRoomTask(
-                            taskKey = TaskKeys.SPRAYING_TASK,
-                            frequency = 2
-                        )
-                    )
-                )
-            )
-            val dao = StubPlantWithTasksDao(initialList)
-
-            val actual = RoomTasksRepository(dao)
-                .getPlantsWithTasksForDate(date)
-                .first()
-                .map { it.second }
-                .flatten()
-
-            val expectedResult = listOf(
-                Task.SprayingTask(2)
-            )
-            assertEquals(expectedResult, actual)
-        }
-
-    @Test
-    fun `empty plants and tasks lists - returns empty list`() = runBlockingTest {
+    fun `when empty plants list - return empty list`() = runBlockingTest {
         val date = createDate(2021, 11, 10)
         val dao = StubPlantWithTasksDao(emptyList())
 
         val actual = RoomTasksRepository(dao)
             .getPlantsWithTasksForDate(date)
             .first()
-            .map { it.second }
-            .flatten()
 
-        val expectedResult = emptyList<Task>()
+        val expectedResult = emptyList<RoomPlantWithTasks>()
         assertEquals(expectedResult, actual)
     }
 
     @Test
-    fun `empty tasks lists - returns empty list`() = runBlockingTest {
+    fun `when plant with no tasks - return empty list`() = runBlockingTest {
         val date = createDate(2021, 11, 10)
         val initialList = listOf(
-            RoomPlantWithTasks(
-                createRoomPlant(creationDateMillis = date.time),
-                emptyList()
-            ),
             RoomPlantWithTasks(
                 createRoomPlant(creationDateMillis = date.time),
                 emptyList()
@@ -203,8 +146,6 @@ class RoomTasksRepositoryTest {
         val actual = RoomTasksRepository(dao)
             .getPlantsWithTasksForDate(date)
             .first()
-            .map { it.second }
-            .flatten()
 
         val expectedResult = emptyList<Pair<Plant, List<Task>>>()
         assertEquals(expectedResult, actual)
