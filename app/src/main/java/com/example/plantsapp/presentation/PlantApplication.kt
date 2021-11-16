33ca5e70b6223f5e0a996d10764db197f285bdb0
Tatsiana Talkachova <tatianatolkachyova@gamil.com>
2021-11-16 11:34:38 +0300

    feat: add notification worker

--- app/src/main/java/com/example/plantsapp/presentation/PlantApplication.kt
+++ app/src/main/java/com/example/plantsapp/presentation/PlantApplication.kt
@@ -1,12 +1,18 @@
 package com.example.plantsapp.presentation
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import com.example.plantsapp.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
-class PlantApplication : Application() {
+class PlantApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
 
     override fun onCreate() {
         super.onCreate()
@@ -15,4 +21,9 @@ class PlantApplication : Application() {
             Timber.plant(Timber.DebugTree())
         }
     }
+
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
--- app/src/main/java/com/example/plantsapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/plantsapp/presentation/ui/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.plantsapp.presentation.ui
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.commit
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.plantsapp.R
 import com.example.plantsapp.databinding.ActivityMainBinding
+import com.example.plantsapp.presentation.ui.notification.NotificationWorker
 import com.example.plantsapp.presentation.ui.plants.PlantsFragment
 import com.example.plantsapp.presentation.ui.tasksfordays.TasksForDaysFragment
+import com.example.plantsapp.presentation.ui.utils.calculateDelay
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Calendar
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -27,6 +35,11 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+        startNotificationWorker(
+            context = applicationContext,
+            startDate = Calendar.getInstance()
+        )
     }
 
     private fun openFragment(fragment: Fragment) {
@@ -34,5 +47,25 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.commit {
             replace(R.id.fragment_container, fragment)
         }
+    }
+
+    private fun startNotificationWorker(context: Context, startDate: Calendar) {
+        val notificationRequest =
+            PeriodicWorkRequestBuilder<NotificationWorker>(1, TimeUnit.DAYS)
+                .setInitialDelay(
+                    startDate.calculateDelay(nextDayHour = HOUR_OF_WORK_STARTING),
+                    TimeUnit.MILLISECONDS
+                )
+                .build()
+
+        WorkManager.getInstance(context).enqueueUniquePeriodicWork(
+            NotificationWorker.NOTIFICATION_WORK_NAME,
+            ExistingPeriodicWorkPolicy.REPLACE,
+            notificationRequest
+        )
+    }
+
+    companion object {
+        private const val HOUR_OF_WORK_STARTING = 10
     }
 }
