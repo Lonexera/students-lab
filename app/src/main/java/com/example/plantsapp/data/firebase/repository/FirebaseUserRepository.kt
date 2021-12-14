08c02929b39288a760c030f44060738d2be72bae
Tatsiana <tatianatolkachyova@gmail.com>
2021-12-14 13:27:04 +0300

    refactor: add sharedPrefs module to di

--- app/src/main/java/com/example/plantsapp/data/firebase/repository/FirebaseUserRepository.kt
+++ app/src/main/java/com/example/plantsapp/data/firebase/repository/FirebaseUserRepository.kt
@@ -1,21 +1,18 @@
 package com.example.plantsapp.data.firebase.repository
 
-import android.content.Context
+import android.content.SharedPreferences
 import com.example.plantsapp.data.firebase.utils.user
 import com.example.plantsapp.domain.model.User
 import com.example.plantsapp.domain.repository.UserRepository
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class FirebaseUserRepository @Inject constructor(
-    @ApplicationContext private val context: Context
+    private val sharedPrefs: SharedPreferences
 ) : UserRepository {
 
-    private val sharedPrefs = context
-            .getSharedPreferences(PREFERENCES_FILE_KEY, Context.MODE_PRIVATE)
-    private var user: User? = sharedPrefs?.user
+    private var user: User? = sharedPrefs.user
 
     override suspend fun setUser(user: User) {
         sharedPrefs.user = user
@@ -30,9 +27,5 @@ class FirebaseUserRepository @Inject constructor(
     override suspend fun clearUser() {
         sharedPrefs.user = null
         user = null
-    }
-
-    companion object {
-        private const val PREFERENCES_FILE_KEY = "com.example.plantsApp.PREFERENCE_FILE_KEY"
     }
 }
