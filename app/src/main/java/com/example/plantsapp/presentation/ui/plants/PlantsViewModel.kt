3482bccce50079df9c55127dd01f0f75469a8d3f
Tatsiana Talkachova <tatianatolkachyova@gamil.com>
2022-01-08 14:40:02 +0300

    feat: add loading to Plants screen

--- app/src/main/java/com/example/plantsapp/presentation/ui/plants/PlantsViewModel.kt
+++ app/src/main/java/com/example/plantsapp/presentation/ui/plants/PlantsViewModel.kt
@@ -12,21 +12,26 @@ import com.example.plantsapp.presentation.ui.utils.combineWith
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
+@Suppress("TooGenericExceptionCaught")
 @HiltViewModel
 class PlantsViewModel @Inject constructor(
     @FirebaseQualifier private val repository: PlantsRepository
 ) : ViewModel() {
 
-    private val allPlants: MutableLiveData<List<Plant>> = MutableLiveData(emptyList())
+    private val allPlants: MutableLiveData<List<Plant>> = MutableLiveData()
     private val filter: MutableLiveData<String> = MutableLiveData("")
     val filteredPlants: LiveData<List<Plant>> = allPlants.combineWith(filter) { plants, filter ->
-        plants!!.filter {
+        plants?.filter {
             it.name.value.contains(filter!!.trim(), ignoreCase = true) ||
                     it.speciesName.contains(filter.trim(), ignoreCase = true)
         }
     }
+
+    private val _isLoading: MutableLiveData<Boolean> = MutableLiveData(false)
+    val isLoading: LiveData<Boolean> get() = _isLoading
 
     private val _clickedPlant: MutableLiveData<Event<Plant>> = MutableLiveData()
     val clickedPlant: LiveData<Event<Plant>> get() = _clickedPlant
@@ -35,8 +40,17 @@ class PlantsViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            repository.observePlants().collect {
-                allPlants.value = it
+            try {
+                _isLoading.value = true
+                repository.observePlants().collect {
+                    allPlants.value = it
+
+                    _isLoading.value = false
+                }
+            } catch (e: Exception) {
+                Timber.e(e)
+            } finally {
+                _isLoading.value = false
             }
         }
     }
